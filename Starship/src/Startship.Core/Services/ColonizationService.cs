73a7b1d490a300523e258339d665aa379924edcd
DannyM <danjmurphy92@gmail.com>
2018-02-23 23:10:29 +0000

    Defined interface for colonization service

---
+++ Starship/src/Startship.Core/Services/ColonizationService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Starship.Core.Models;
+using Starship.Core.Services.Interfaces;
+
+namespace Starship.Core.Services
+{
+    public class ColonizationService : IColonizationService
+    {
+        public IEnumerable<Planet> ConquerTheUniverseSecs(Position startPosition, IEnumerable<Planet> planets, int timeInSecs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Planet> ConquerTheUniverseMins(Position startPosition, IEnumerable<Planet> planets, int timeInMins)
+        {
+            return ConquerTheUniverseMins(startPosition, planets, timeInMins * 60);
+        }
+
+        public IEnumerable<Planet> ConquerTheUniverseHours(Position startPosition, IEnumerable<Planet> planets, int timeInHours)
+        {
+            return ConquerTheUniverseMins(startPosition, planets, timeInHours * 60);
+        }
+    }
+}
---
+++ Starship/src/Startship.Core/Services/Interfaces/IColonizationService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Starship.Core.Models;
+using Starship.Core.Models.Abstracts;
+
+namespace Starship.Core.Services.Interfaces
+{
+    public interface IColonizationService
+    {
+        IEnumerable<Planet> ConquerTheUniverseSecs(Position startPosition, IEnumerable<Planet> planets, int timeInSecs);
+
+        IEnumerable<Planet> ConquerTheUniverseMins(Position startPosition, IEnumerable<Planet> planets, int timeInMins);
+
+        IEnumerable<Planet> ConquerTheUniverseHours(Position startPosition, IEnumerable<Planet> planets, int timeInHours);
+    }
+}
